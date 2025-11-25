75705d81c1e59076a66ad51c21d755d4563f4232
RaposoBrendo <raposobrendo@gmail.com>
2025-11-25 17:08:50 -0300

    First commit. Remembering encapsulation and This statement.

---
+++ src/application/ProgramProduct.java
@@ -0,0 +1,36 @@
+package application;
+
+import entities.Product;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ProgramProduct{
+    public static void main(String[] Args){
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter product data:");
+        System.out.print("Name: ");
+        String name = sc.nextLine();
+        System.out.print("Price: ");
+        double price = sc.nextDouble();
+        System.out.print("Quantity: ");
+        int quantity = sc.nextInt();
+        Product product = new Product(name, price, quantity);
+
+        System.out.println("Product data: " + product);
+        System.out.print("Enter the number of products to be added in stock: ");
+        quantity = sc.nextInt();
+        product.addProducts(quantity);
+        System.out.println("Updated data: " + product);
+        System.out.print("Enter the number of products to be removed in stock: ");
+        quantity = sc.nextInt();
+        product.removeProducts(quantity);
+        System.out.println("Updated data: " + product);
+
+
+        sc.close();
+    }
+}
+
